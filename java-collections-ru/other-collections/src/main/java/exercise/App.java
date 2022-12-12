0cc8c091900cd4bebdb00521e428bc9a71af18d1
@hexlet/cli <support@hexlet.io>
2022-12-12 22:11:47 +0300

    submit java-collections-ru/other-collections

--- java-collections-ru/other-collections/src/main/java/exercise/App.java
+++ java-collections-ru/other-collections/src/main/java/exercise/App.java
@@ -1,36 +1,27 @@
 package exercise;
 
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
-import java.util.TreeSet;
+import java.util.*;
 
 // BEGIN
 class App {
-	public static void main(String[] args) {
-		Map<String, Object> data1 = new HashMap<>(Map.of("one", "eon", "two", "two", "four", true));
-		Map<String, Object> data2 = new HashMap<>(Map.of("two", "own", "zero", 4, "four", true));
-		Map<String, String> result = App.genDiff(data1, data2);
-		System.out.println(result);
-	}
 
-	public static Map<String, String> genDiff(Map<String,Object> data1, Map<String,Object> data2) {
-		Map<String, String> result = new TreeMap<>(Comparator.naturalOrder());
-		for (String k1 : data1.keySet()) {
-			for (String k2: data2.keySet()) {
-				if(data1.containsKey(k2) && data2.containsKey(k1) && data1.get(k1).equals(data2.get(k2))) {
-					result.put(k1, "unchanged");
-				} else if (data1.containsKey(k2) && data2.containsKey(k1) && !data1.get(k1).equals(data2.get(k2))) {
-					result.put(k1, "changed");
-				}
-				if (!data1.containsKey(k2) && data2.containsKey(k2)) result.put(k2, "added");
-				if (data1.containsKey(k1) && !data2.containsKey(k1)) result.put(k1, "deleted");
-			}
-		}
-		return result;
-	}
+
+    public static Map<String, String> genDiff(Map<String, Object> data1, Map<String, Object> data2) {
+        Map<String, String> result = new TreeMap<>(Comparator.naturalOrder());
+        HashSet<String> keySet = new HashSet<>();
+        keySet.addAll(data1.keySet());
+        keySet.addAll(data2.keySet());
+
+        for (String k : keySet) {
+            if (data1.containsKey(k) && data2.containsKey(k) && data1.get(k).equals(data2.get(k)))
+                result.put(k, "unchanged");
+            if (data1.containsKey(k) && data2.containsKey(k) && !data1.get(k).equals(data2.get(k)))
+                result.put(k, "changed");
+            if (data1.containsKey(k) && !data2.containsKey(k)) result.put(k, "deleted");
+            if (data2.containsKey(k) && !data1.containsKey(k)) result.put(k, "added");
+        }
+
+        return result;
+    }
 }
 //END
